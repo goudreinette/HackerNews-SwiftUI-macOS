134040436cec9972935e5a6578cecdabca99df61
Rein van der Woerd <reinvanderwoerd@me.com>
2019-10-14 22:52:41 +0200

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Rein van der Woerd on 14/10/2019.
+//  Copyright © 2019 Rein van der Woerd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
